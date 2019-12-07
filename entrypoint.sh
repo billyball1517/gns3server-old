d2b982fd4f204143d97f1505fcc782b7e9faa914
billyball1517 <54004311+billyball1517@users.noreply.github.com>
2019-12-07 18:06:29 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+# Add local user
+# Either use the LOCAL_USER_ID if passed in at runtime or
+# fallback
+
+USER_ID=${LOCAL_USER_ID:-9001}
+
+echo "Starting with UID : $USER_ID"
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+useradd -m -s /bin/bash -u $USER_ID -o user
+export HOME=/home/user
+
+usermod -aG ubridge user
+usermod -aG libvirt user
+usermod -aG kvm user
+usermod -aG docker user
+
+exec /usr/sbin/gosu user "$@"
