7777b2e8bf181d3de718d04e2ad228539ec0b7eb
billyball1517 <54004311+billyball1517@users.noreply.github.com>
2019-12-07 18:06:51 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,6 @@
 USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
 useradd -m -s /bin/bash -u $USER_ID -o user
 export HOME=/home/user
 
