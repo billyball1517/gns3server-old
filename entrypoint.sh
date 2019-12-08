23e9d297f07c0babe6bc7bc99a8812638d713b5f
billyball1517 <54004311+billyball1517@users.noreply.github.com>
2019-12-08 11:57:36 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,16 @@
 # fallback
 
 USER_ID=${LOCAL_USER_ID:-9001}
+GROUP_ID=${LOCAL_GROUP_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
 useradd -m -s /bin/bash -u $USER_ID -o user
 export HOME=/home/user
 
+groupadd -g -o $GROUP_ID group
+
 usermod -aG ubridge user
 usermod -aG libvirt user
-usermod -aG kvm user
+usermod -aG group user
 
-service libvirtd start
-
-exec /usr/sbin/gosu root "$@"
+exec /usr/sbin/gosu user "$@"
